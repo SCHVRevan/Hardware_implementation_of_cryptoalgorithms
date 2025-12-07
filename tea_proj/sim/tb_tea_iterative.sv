85fffbf045a4fdc52856de877dd3a31b7b55d909
Egor <120785170+SCHVRevan@users.noreply.github.com>
2025-12-07 16:10:24 +0700

    Update tb_tea_iterative.sv

--- tea_proj/sim/tb_tea_iterative.sv
+++ tea_proj/sim/tb_tea_iterative.sv
@@ -3,6 +3,11 @@
 module tb_tea_iterative;
 	parameter VALIDATIONS = 100;
 	
+   	logic s_axis_tvalid;
+  	logic s_axis_tready;
+  	logic m_axis_tvalid;
+  	logic m_axis_tready;
+
 	logic rst = 0;
 	logic clk = 1;
 	logic [255:0] test_vectors [VALIDATIONS];
@@ -25,11 +30,15 @@ initial begin
 	for (int i = 0; i < VALIDATIONS; i++) begin
 		{data, key, tea_ref} = test_vectors[i];
 		
-		repeat(34) @(posedge clk);
+		wait(s_axis_tready);
+
+		while (!m_axis_tvalid) @(posedge clk);
+		m_axis_tready = 1'b1;
+		@(posedge clk);
+		m_axis_tready = 1'b0;
 
 		assert (tea == tea_ref) else $error("Wrong encrypted data on test vector %d: tea = %h, tea_ref = %h", i, tea, tea_ref);
 	end
-
 	$finish;
 end
 
@@ -39,12 +48,14 @@ tea_iterative dut (
   .rst (rst),
   .clk (clk),
   .key (key),
+
   .s_axis_tdata (data),
-  .s_axis_tvalid (1'b1),  // всегда валидный вход
-  .s_axis_tready (),      // не используется
+  .s_axis_tvalid (1'b1),
+  .s_axis_tready (s_axis_tready),
+
   .m_axis_tdata (tea),
-  .m_axis_tvalid (),      // не используется
-  .m_axis_tready (1'b1)   // всегда готов принимать
+  .m_axis_tvalid (m_axis_tvalid),
+  .m_axis_tready (m_axis_tready)
 );
 
 endmodule
